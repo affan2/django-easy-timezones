88f37bdb180733e7ae4d13fde5b0498a2cc4717e
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-09-15 15:32:11 +0400

    Fix bugs in the urlconf, part 1

--- easy_timezones/urls.py
+++ easy_timezones/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^without_tz/$', views.without_tz, name="without_tz"),
-    url(r'^with_tz/$', views.with_tz, name="with_tz"),
+    re_path(r'^without_tz/$', views.without_tz, name="without_tz"),
+    re_path(r'^with_tz/$', views.with_tz, name="with_tz"),
 ]
